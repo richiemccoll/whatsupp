8d459842acdfebf582eadcd6dbc743c925fe25e7
Richie McColl <richie.mccoll@hotmail.co.uk>
2019-07-19 13:38:27 +0100

    Add useMemo in client to fetch the mock data.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 const App: React.FC = () => {
+  const [chats, setChats] = useState<any[]>([]);
+
+  useMemo(async () => {
+    const body = await fetch('/chats');
+    const chats = await body.json();
+    setChats(chats);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header"></header>
+      <section>{JSON.stringify(chats)}</section>
     </div>
   );
 };
