bb1a5c6f6b0c349a79fe271314a45e2074c1ed22
Richie McColl <richie.mccoll@hotmail.co.uk>
2019-07-19 15:58:46 +0100

    server: update mock data and add ts-node-dev.

--- server/mocks/chats.ts
+++ server/mocks/chats.ts
@@ -1,26 +1,34 @@
+import { messages } from './messages';
+
 export const chats = [
   {
     id: '1',
     name: 'Ethan Gonzalez',
     picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
-    lastMessage: '1',
+    lastMessage: messages.find(m => m.id === 1),
   },
   {
     id: '2',
     name: 'Bryan Wallace',
     picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
-    lastMessage: '2',
+    lastMessage: messages.find(m => m.id === 2),
   },
   {
     id: '3',
     name: 'Avery Stewart',
     picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
-    lastMessage: '3',
+    lastMessage: messages.find(m => m.id === 3),
   },
   {
     id: '4',
     name: 'Katie Peterson',
     picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-    lastMessage: '4',
+    lastMessage: messages.find(m => m.id === 4),
+  },
+  {
+    id: '5',
+    name: 'John McPherson',
+    picture: 'https://randomuser.me/api/portraits/thumb/men/4.jpg',
+    lastMessage: messages.find(m => m.id === 5),
   },
 ];
---
+++ server/mocks/messages.ts
@@ -0,0 +1,27 @@
+export const messages = [
+  {
+    id: 1,
+    content: 'You on your way?',
+    createdAt: new Date(Date.now() - 60 * 1000 * 1000),
+  },
+  {
+    id: 2,
+    content: "Hey, it's me",
+    createdAt: new Date(Date.now() - 2 * 60 * 1000 * 1000),
+  },
+  {
+    id: 3,
+    content: 'I should buy a boat',
+    createdAt: new Date(Date.now() - 24 * 60 * 1000 * 1000),
+  },
+  {
+    id: 4,
+    content: 'This is wicked good ice cream.',
+    createdAt: new Date(Date.now() - 14 * 24 * 60 * 1000 * 1000),
+  },
+  {
+    id: 5,
+    content: 'Rust is a great programming language.',
+    createdAt: new Date(Date.now() - 15 * 24 * 60 * 1000 * 1000),
+  },
+];
